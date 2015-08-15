574a9db855b8314428b4ce8500d5c572ee14c1e3
Eric <omnicrola@gmail.com>
2015-08-15 16:35:05 -0400

    flipped Y axis based on Parameter range

--- FcsGraphics/src/com/omnicrola/fcs/image/EventGenerator.java
+++ FcsGraphics/src/com/omnicrola/fcs/image/EventGenerator.java
@@ -8,6 +8,15 @@ import com.omnicrola.fcs.data.Parameter;
 import com.omnicrola.fcs.data.Sample;
 
 public class EventGenerator {
+
+	private static final byte[] ONE = createOne();
+
+	private static byte[] createOne() {
+		final byte[] bytes = new byte[4];
+		bytes[3] = 1;
+		return bytes;
+	}
+
 	private final Sample sample;
 	private final DataBitShifter dataBitShifter;
 	private final ByteBuffer byteBuffer;
@@ -28,16 +37,22 @@ public class EventGenerator {
 	public void createEventAtCoordinate(int x, int y) {
 		this.byteBuffer.clear();
 		final int scale = 100;
+		final byte[] yValue = this.dataBitShifter.translateFromInteger(flipYAxis(y * scale));
+		final byte[] xValue = this.dataBitShifter.translateFromInteger(x * scale);
 		for (final Parameter parameter : this.parameters) {
 			if (parameter.equals(this.xParam)) {
-				this.byteBuffer.put(this.dataBitShifter.translateFromInteger(x * scale));
+				this.byteBuffer.put(xValue);
 			} else if (parameter.equals(this.yParam)) {
-				this.byteBuffer.put(this.dataBitShifter.translateFromInteger(y * scale));
+				this.byteBuffer.put(yValue);
 			} else {
-				this.byteBuffer.put(this.dataBitShifter.translateFromInteger(0));
+				this.byteBuffer.put(ONE);
 			}
 		}
 		this.sample.addEvent(this.byteBuffer.array());
+	}
+
+	private int flipYAxis(int value) {
+		return (int) (this.yParam.getMaxRange() - value);
 	}
 
 }
